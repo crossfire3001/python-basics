217305a2018f9479acd2422069a54163c03ab0de
A Nazarov <roman_emperor5@mail.ru>
2022-10-23 16:04:09 +0500

    Added Data types

--- python + django/datatypes.py
+++ python + django/datatypes.py
@@ -1,1 +1,9 @@
-print('hi')
+# Name: Interger, Type: Int, Description and example: 3, 79, 0, -35;
+# Name: Floating point, Type: float, Description and example: 7.98, -0.905, 1.4;
+# Name: String, Type: str, Description and example: Order sequence of characters: "Hi!", "Jack", "123";
+# Name: Dictionary, Type: dic, Description and example: Unordered "key-value" pairs: {"luckyNumber" : "777", 'name' : 'Jim Huckley'};
+# Name: Tuple, Type: tip, Description and example: Ordered and immutable sequence of objects: ("Hello", 98, 0.5, 98);
+# Name: Set, Type: set, Description and example: Unordered collection of unique objects: ("hello", 98, 0.5);
+# Name: Boolean, Type: bool, Description and example: Logical value: true, false;
+
+
