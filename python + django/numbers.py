91656c7f24115d07ee4c868082ce7ab22c4c041f
A Nazarov <roman_emperor5@mail.ru>
2022-10-23 21:47:43 +0500

    Numbers

---
+++ python + django/numbers.py
@@ -0,0 +1,11 @@
+print(9 + 5) # 14
+print(10 - 8) # 2
+print(8 * 8) # 64
+print(5 / 5) # 1
+print(7 ** 3) # 343
+print(10 % 6) # 4
+
+# homework
+print(3 + 7) # 10
+print(4 ** 3) # 64
+print(29 / 5) # 5.8
