9f2c88cea1099758cd32926aa9f677a079956852
crossfire3001 <obiwan2035@mail.ru>
2022-10-28 21:23:49 +0500

    Calculation of credit in python

--- python + django/varibles.py
+++ python + django/varibles.py
@@ -23,3 +23,12 @@ x = 5
 y = 3
 
 print(x + y)
+
+# Calculation of credit final sum
+
+credit = 1000
+credit_rate = 7
+number_of_years = 5
+final_sum = credit + credit / 100 * credit_rate * number_of_years
+
+print(final_sum) # 1350
