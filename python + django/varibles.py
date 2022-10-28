80efad30fd56f948c167b4dbe939f1ed42ceac90
crossfire3001 <obiwan2035@mail.ru>
2022-10-28 11:50:19 +0500

    1

---
+++ python + django/varibles.py
@@ -0,0 +1,20 @@
+x = 5
+print(x)
+x = "Wat Zin Tai!"
+print(x)
+# x = 7
+# x = 7.3
+type_of_variable = type(x)
+print(type_of_variable)
+
+# Naming Styles
+
+# Type | Naming Convention | Examples
+
+# Function | Use a lowercase word or words. Separate words by underscores to improve readability. | function, my_function
+# Variable | Use a lowercase single letter, word, or words. Separate words with underscores to improve readability. | x, var, my_variable
+# Class	| Start each word with a capital letter. Do not separate words with underscores. This style is called camel case or pascal case. | Model, MyClass
+# Method | Use a lowercase word or words. Separate words with underscores to improve readability. | class_method, method
+# Constant | Use an uppercase single letter, word, or words. Separate words with underscores to improve readability. | CONSTANT, MY_CONSTANT, MY_LONG_CONSTANT
+# Module | Use a short, lowercase word or words. Separate words with underscores to improve readability. | module.py, my_module.py
+# Package | Use a short, lowercase word or words. Do not separate words with underscores. | package, mypackage
